d5475d4dee2b7f74cb49cabb8b4084cf8eb3b808
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-09-10 22:34:45 -0300

    faltan cosas

--- proyectodj.py
+++ proyectodj.py
@@ -1,27 +1,39 @@
 from operator import inv
 import speech_recognition as sr
-import os 
+import os
 import difflib as dl
+import playsound
 
-def main():
-    audio = speech_recognition()
-    path = 'D:\Descargas\backup\Internet Explorer'
-    lista= os.listdir(path)
-    tema = dl.get_close_matches(audio,lista)
-    invertedslash = '\ '
-    invertedslash = invertedslash - ' '
-    path2 = path + invertedslash + tema
-    print(invertedslash)
-    print(tema)
-    print(path2)
-    os.open(path2)
-
-
-main()
-
-def speech_recognition():
+def speech_recorder():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say something!")
         audio = r.listen(source)
     return audio
+
+def speech_recognize(audio):
+    return 'Camellia'
+
+def main():
+    #audio = speech_recorder()
+    audio = speech_recognize('audio')
+    print(audio)
+    lista= os.listdir('D:\\Descargas\\backup\\Internet Explorer')
+    #print(lista)
+    tema = dl.get_close_matches(audio,lista,1,0.1)
+    print(tema)
+    
+    invertedslash = '\\'
+
+    print(invertedslash)
+    path2 = 'D:\\Descargas\\backup\\Internet Explorer' + invertedslash + tema[0]
+    
+    print(path2)
+    playsound.playsound(os.path.relpath(path2))
+
+main()
+
+#TODOLIST
+#1 - RECONOCIMIENTO DE AUDIO
+#2 - PATH RELATIVO
+#3 - EL GET CLOSE MATCHES HACE CUALQUIER COSA

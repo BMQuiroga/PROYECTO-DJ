cec292702c77ed9a99229c389985153d5a30350c
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-09-11 15:35:13 -0300

    finiquitado parcial

--- proyectodj.py
+++ proyectodj.py
@@ -1,39 +1,53 @@
-from operator import inv
+from logging import exception
 import speech_recognition as sr
 import os
 import difflib as dl
-import playsound
 import webbrowser
+
 
 def speech_recorder():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say something!")
         audio = r.listen(source)
-    return audio
+        try:
+            speech = r.recognize_google(audio)
+            
+            
+            #print('Google: ' + speech)
+            
+            
+            #print('Bing: ' + r.recognize_bing(audio,))REQUIERE KEY
+            #print('Houndify: ' + r.recognize_houndify(audio))REQUIERE ID Y KEY
+            #print('IBM: ' + r.recognize_ibm(audio)) REQUIERE USERNAME Y PASSWORD
+            #print('Sphinx: ' + r.recognize_sphinx(audio)) MISSING POCKETSPINX MODULE
+            #print('Wit: ' + r.recognize_wit(audio)) REQUIERE KEY
+        except Exception as e:
+            print('Error' + str(e))
+    
+    return speech
 
-def speech_recognize(audio):
-    return 'Camellia'
 
 def main():
-    #audio = speech_recorder()
-    audio = speech_recognize('audio')
-    print(audio)
-    lista= os.listdir('D:\\Descargas\\backup\\Internet Explorer')
+    audio = speech_recorder()
+    
+    #print(audio)
+    lista = os.listdir('D:\\Descargas\\backup\\Internet Explorer')
     #print(lista)
     tema = dl.get_close_matches(audio,lista,1,0.1)
-    print(tema)
+    #print(tema)
     
     invertedslash = '\\'
 
-    print(invertedslash)
+    #print(invertedslash)
     path2 = 'D:\\Descargas\\backup\\Internet Explorer' + invertedslash + tema[0]
     
-    print(path2)
-    #playsound.playsound(os.path.relpath(path2))
+    #print(path2)
+    
     webbrowser.open(path2)
 
-main()
+if __name__ == '__main__':
+    main()
 
 #TODOLIST
 #1 - RECONOCIMIENTO DE AUDIO

ce8f42d6091be9c3745f77fd3434816f73cb589f
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-09-10 21:47:31 -0300

    terminado sin probar

--- proyectodj.py
+++ proyectodj.py
@@ -1,8 +1,27 @@
+from operator import inv
 import speech_recognition as sr
 import os 
 import difflib as dl
 
 def main():
-    print('a')   
+    audio = speech_recognition()
+    path = 'D:\Descargas\backup\Internet Explorer'
+    lista= os.listdir(path)
+    tema = dl.get_close_matches(audio,lista)
+    invertedslash = '\ '
+    invertedslash = invertedslash - ' '
+    path2 = path + invertedslash + tema
+    print(invertedslash)
+    print(tema)
+    print(path2)
+    os.open(path2)
+
 
 main()
+
+def speech_recognition():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    return audio

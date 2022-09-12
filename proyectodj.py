3de0b27a3eb317dfbe899eb361c8977ae546671f
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-09-12 12:21:10 -0300

    empezando la ui

--- proyectodj.py
+++ proyectodj.py
@@ -1,8 +1,45 @@
+from email.mime import image
 from logging import exception
+
 import speech_recognition as sr
 import os
 import difflib as dl
 import webbrowser
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1024,696))
+
+def commands(numero):
+    pass
+
+
+
+class boton():
+    def __init__(self,x,y,img,size):
+        
+        self.x = x
+        self.y = y
+        self.width = img.get_width()
+        self.height = img.get_height()
+        self.img = pygame.transform.scale(img,(int(self.width * size),int(self.height * size)))
+        self.rect = self.img.get_rect()
+        self.rect.topleft = (x,y)
+        self.click = False
+
+    def draw(self):
+        action = False
+        screen.blit(self.img,[self.x,self.y])
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            if pygame.mouse.get_pressed()[0] and not self.click:
+                self.click = True
+                action = True
+        if not pygame.mouse.get_pressed()[0]:
+            self.click = False
+            action = False
+        return action
 
 
 def speech_recorder():
@@ -28,7 +65,10 @@ def speech_recorder():
     return speech
 
 
-def main():
+def main_audio():
+
+
+
     audio = speech_recorder()
     
     #print(audio)
@@ -46,9 +86,45 @@ def main():
     
     webbrowser.open(path2)
 
+
+def main():
+    
+
+    arial = pygame.font.SysFont('Arial', 100)
+    texto_si = arial.render('Si',False,(0,0,0))
+    texto_no = arial.render('No',False,(0,0,0))
+    rojo = (255,0,0)
+    background = pygame.image.load("D:\Documentos\git\PROYECT\PROYECTO-DJ\dj.jpg").convert()
+    button = pygame.image.load("D:\Documentos\git\PROYECT\PROYECTO-DJ\\button_re.png").convert()
+
+    boton_si = boton(300,440,button,1)
+    boton_no = boton(1024-300-158,440,button,1)
+
+    while 1:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            
+
+        screen.blit(background,[0,0])
+
+
+        if boton_no.draw():
+            exit()
+        if boton_si.draw():
+            print('si')
+
+        screen.blit(texto_si,[300,400])
+        screen.blit(texto_no,[1024-300-158,400])
+
+
+        #screen.blit(button,[300,440])
+        #screen.blit(button,[1024-300-158,440])
+        pygame.display.flip()
+
+
 if __name__ == '__main__':
     main()
 
-#TODOLIST
-#1 - RECONOCIMIENTO DE AUDIO
-#3 - EL GET CLOSE MATCHES HACE CUALQUIER COSA
+
